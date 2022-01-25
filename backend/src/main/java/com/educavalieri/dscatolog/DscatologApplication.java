27eeaac2e36c28fd93f1803c568a058780739028
educavalieri <eduardocavalieri@hotmail.com>
2022-01-25 12:01:47 -0300

    Backend project created

---
+++ backend/src/main/java/com/educavalieri/dscatolog/DscatologApplication.java
@@ -0,0 +1,13 @@
+package com.educavalieri.dscatolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatologApplication.class, args);
+	}
+
+}
