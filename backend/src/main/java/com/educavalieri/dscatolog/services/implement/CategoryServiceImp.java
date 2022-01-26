784b96760d0375b8482bf62d6534426bd23202d2
educavalieri <eduardocavalieri@hotmail.com>
2022-01-26 10:18:40 -0300

    implement seeding SQL

--- backend/src/main/java/com/educavalieri/dscatolog/services/implement/CategoryServiceImp.java
+++ backend/src/main/java/com/educavalieri/dscatolog/services/implement/CategoryServiceImp.java
@@ -5,6 +5,7 @@ import com.educavalieri.dscatolog.repositories.CategoryRepository;
 import com.educavalieri.dscatolog.services.interfaces.CategoryServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,7 +16,9 @@ public class CategoryServiceImp implements CategoryServiceInterface {
     private CategoryRepository categoryRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
+
         return categoryRepository.findAll();
     }
 }
