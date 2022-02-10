650f3fbb6c8c0451652795370036f52bcf5d9d0b
educavalieri <eduardocavalieri@hotmail.com>
2022-02-10 15:30:31 -0300

    create environment variables

--- backend/src/main/java/com/educavalieri/dscatolog/config/AppConfig.java
+++ backend/src/main/java/com/educavalieri/dscatolog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.educavalieri.dscatolog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCrypt;
@@ -10,6 +11,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+    @Value("${jwt.secret}")
+    private String jwtsecret;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -18,7 +22,7 @@ public class AppConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("MY-JWT-SECRET");
+        tokenConverter.setSigningKey(jwtsecret);
         return tokenConverter;
     }
 
