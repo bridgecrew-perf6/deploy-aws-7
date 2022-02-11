de0c7d57e30a81522062898c5f1edacdfaeadfef
educavalieri <eduardocavalieri@hotmail.com>
2022-02-11 15:57:18 -0300

    H2 security configuration

--- backend/src/main/java/com/educavalieri/dscatolog/config/WebSecurityConfig.java
+++ backend/src/main/java/com/educavalieri/dscatolog/config/WebSecurityConfig.java
@@ -22,20 +22,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
     }
 
     @Autowired
-        private BCryptPasswordEncoder passwordEncoder;
+    private BCryptPasswordEncoder passwordEncoder;
 
-        @Autowired
-        private UserDetailsService userDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-        }
-
-        @Override
-        public void configure(WebSecurity web) throws Exception {
-            web.ignoring().antMatchers("/actuator/**");
-        }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+       web.ignoring().antMatchers("/actuator/**");
+    }
+}
 
 
--- backend/src/main/java/com/educavalieri/dscatolog/dto/UserUpdateDTO.java
+++ backend/src/main/java/com/educavalieri/dscatolog/dto/UserUpdateDTO.java
@@ -1,6 +1,5 @@
 package com.educavalieri.dscatolog.dto;
 
-import com.educavalieri.dscatolog.services.validation.UserInsertValid;
 import com.educavalieri.dscatolog.services.validation.UserUpdateValid;
 
 @UserUpdateValid
